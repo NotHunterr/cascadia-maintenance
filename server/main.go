c20481c5e91a13c45a90b500b2b0be946992e35c
Hunter <notryoshi@gmail.com>
2024-03-24 21:49:54 -0700

    Working on bug fix

--- server/main.go
+++ server/main.go
@@ -8,22 +8,25 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
 
-	fs := http.FileServer(http.Dir("../views"))
-	router.Handle("/*", http.StripPrefix("/", fs))
-	router.Get("/validLogin", validLoginHandler)
-	http.ListenAndServe("localhost:42069", router)
 
-}
 
 func validLoginHandler(w http.ResponseWriter, r *http.Request) {
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-
-	if username == "admin" && password == "admin" {
-		fmt.Println("Login successful")
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+	
+		fmt.Printf("Username: %s", username)
+		fmt.Printf("Password: %s", password)
 	}
-}
+
+
+	func main() {
+		router := chi.NewRouter()
+		router.Use(middleware.Logger)
+	
+		fs := http.FileServer(http.Dir("../views"))
+		router.Handle("/*", http.StripPrefix("/", fs))
+		router.Get("/validLogin", validLoginHandler)
+		fmt.Println("Server is running on port 42069")
+		http.ListenAndServe("localhost:42069", router)
+	}
