67a35f55de9b0734fd62bb08f2442a8c92d0be26
Hunter <notryoshi@gmail.com>
2024-03-25 18:47:12 -0700

    Testing out golang temaplates

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -9,24 +10,35 @@ import (
 )
 
 
+type Templates struct {
+	templates *template.Template
+}
 
+func (t *Templates) RenderTemplate(w http.ResponseWriter, name string, data interface{}) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("../views/*.html")),
+	}
+}
 
 func validLoginHandler(w http.ResponseWriter, r *http.Request) {
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-	
-		fmt.Printf("Username: %s", username)
-		fmt.Println("Password: ", password)
-		fmt.Println(":D")
-	}
+	username := r.FormValue("username")
+	password := r.FormValue("password")
 
+	fmt.Printf("Username: %s", username)
+	fmt.Println("Password: ", password)
+	fmt.Fprintf(w, "<p>Login Successful!!!!</p>")
+}
 
-	func main() {
+func main() {
 		router := chi.NewRouter()
 		router.Use(middleware.Logger)
 	
-		fs := http.FileServer(http.Dir("../views"))
-		router.Handle("/*", http.StripPrefix("/", fs))
+		// fs := http.FileServer(http.Dir("../views"))
+		// router.Handle("/*", http.StripPrefix("/", fs))
 		router.Get("/validLogin", validLoginHandler)
 		fmt.Println("Server is running on port 42069")
 		http.ListenAndServe("localhost:42069", router)
